78548d63990df13adbf2b8045a2718b339cabfd4
IPhone Hlifone IPad Developer <hlifone@IPhone--IPad-Developer.local>
2014-07-07 22:21:30 -0700

    added test class

---
+++ Git test/OPTest.h
@@ -0,0 +1,13 @@
+//
+//  OPTest.h
+//  Git test
+//
+//  Created by IPhone & IPad Developer on 7/7/14.
+//  Copyright (c) 2014 IPhone & IPad Developer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPTest : NSObject
+
+@end
---
+++ Git test/OPTest.m
@@ -0,0 +1,13 @@
+//
+//  OPTest.m
+//  Git test
+//
+//  Created by IPhone & IPad Developer on 7/7/14.
+//  Copyright (c) 2014 IPhone & IPad Developer. All rights reserved.
+//
+
+#import "OPTest.h"
+
+@implementation OPTest
+
+@end
